94014ed2719c01117db5ad06e17f815e947a7a98
Abishek Mahi <abishekmas0708@gmail.com>
2024-12-01 15:43:14 +0530

    design chages

--- index.js
+++ index.js
@@ -5,39 +5,35 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-let blogList=[];
+let blogList = [];
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.render("index.ejs");
-   
 });
 
-app.get("/home",(req,res) =>{
+app.get("/home", (req, res) => {
     res.render("blogList.ejs", {
         blogList: blogList
     });
 });
 
+app.post("/home", (req, res) => {
+    const blogTitle = req.body.blogTitle;
+    const blogdes = req.body.blogDes;
 
+    // Check if a blog with the same title and description already exists
+    const blogExists = blogList.some((blog) => blog.title === blogTitle && blog.description === blogDes);
 
-app.post("/home",(req,res) =>{
-    const blogTitle=req.body.blogTitle;
-    const blogdes=req.body.blogDes;
-
-      // Check if a blog with the same title and description already exists
-      const blogExists = blogList.some((blog) => blog.title === blogTitle && blog.description === blogDes);
-
-      // If the blog already exists, don't add it again
-      if (blogExists) {
-          return res.render("blogList.ejs", {
-              blogList: blogList, // Render the list without adding a duplicate blog
-          });
-      }
+    // If the blog already exists, don't add it again
+    if (blogExists) {
+        return res.render("blogList.ejs", {
+            blogList: blogList, // Render the list without adding a duplicate blog
+        });
+    }
 
     blogList.push({
         id: generateID(),
@@ -49,69 +45,69 @@ app.post("/home",(req,res) =>{
         blogList: blogList
     });
 
-    
+
 });
 
 
 
-app.get("/blogDetails/:id", (req,res) =>{
+app.get("/blogDetails/:id", (req, res) => {
     const blogId = req.params.id;
     const blogDetails = blogList.find((blog) => blog.id === parseInt(blogId));
 
-    res.render("viewBlogDetails.ejs",{
+    res.render("viewBlogDetails.ejs", {
         blogDetails: blogDetails
     });
 });
 
 
-app.get("/edit/:id", (req,res) =>{
+app.get("/edit/:id", (req, res) => {
 
     const blogId = req.params.id;
     const blogDetails = blogList.find((blog) => blog.id === parseInt(blogId));
     res.render("index.ejs", {
-      isEdit: true,
-      blogDetails: blogDetails,
+        isEdit: true,
+        blogDetails: blogDetails,
     });
 })
 
 
-app.post("/edit/:id", (req,res)=>{
-    const blogId =req.params.id;
+app.post("/edit/:id", (req, res) => {
+    const blogId = req.params.id;
     const editBlog = blogList.findIndex((blog) => blog.id === parseInt(blogId));
-    if(editBlog===-1){
+    if (editBlog === -1) {
         res.send("<h1> Something went wrong </h1>");
 
     }
 
-    const updatedTitle = req.body.blogTitle;                                
-     
-    const updatedDescription = req.body.blogDes; 
+    const updatedTitle = req.body.blogTitle;
 
-    
-      blogList[editBlog].title = updatedTitle;
-     blogList[editBlog].description = updatedDescription;
+    const updatedDescription = req.body.blogDes;
 
-     res.render("blogList.ejs", {
-        isEdit:true,
-        blogList:blogList,
-     })
+
+    blogList[editBlog].title = updatedTitle;
+    blogList[editBlog].description = updatedDescription;
+
+    res.render("blogList.ejs", {
+        isEdit: true,
+        blogList: blogList,
+    })
 
 })
 
-app.post("/delete/:id", (req,res) =>{
-    const blogId=req.params.id;
+app.post("/delete/:id", (req, res) => {
+    const blogId = req.params.id;
 
     blogList = blogList.filter((blog) => blog.id !== parseInt(blogId));
-res.send( '<script> alert("Blog deleted successfully!");window.location="/home"</script>');
+    res.send('<script> alert("Blog deleted successfully!");window.location="/home"</script>');
 
-//res.redirect("/home");
+    //res.redirect("/home");
 
 })
 
 
 
-function generateID(){
-return Math.floor(Math.random() *1000);
+function generateID() {
+    return Math.floor(Math.random() * 1000);
 }
 app.listen(port, () => {
     console.log(`Server is running on port ${port}
